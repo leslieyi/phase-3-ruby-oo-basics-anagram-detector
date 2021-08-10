4d7f4e773e46866df0534122973fac00c48e38c9
leslieyi <leslieyi411@gmail.com>
2021-08-10 16:55:48 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def broken_word
+        @word.split("").sort
+    end
+    def broken_array(word)
+        word.split("").sort
+    end
+    def match(array)
+        array.select{|word| word.split("").sort == @word.split("").sort}
+    end
+end
